58e4ee12e9ed435e294c77ec592cd33c99d735d9
loic cessot <loic.cessot@gadz.org>
2017-05-04 12:14:44 +0200

    add color to task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504095724) do
+ActiveRecord::Schema.define(version: 20170504100711) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170504095724) do
     t.integer "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 
 end
